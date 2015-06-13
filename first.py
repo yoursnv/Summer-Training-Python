e7992153667f31efd1f2a1153b7b3b1ccd05f4d1
nitinvkttn <nitinvkttn@gmail.com>
2015-06-13 10:30:56 +0530

    Delete first.py

--- first.py
+++
@@ -1,8 +0,0 @@
-def move(time, speed):
-	print("I am moving on "+ str(speed) + " Kmph " + " at " + str(time))
-	x = time + 1
-	y = speed - 1
-	print("I am moving on "+ str(y) + " Kmph " + " at " + str(x))
-
-
-move(18,5)
